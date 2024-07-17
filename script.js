c0627af501c61774f3a61e9c2436ef3bb77d533e
BLACK1BAmwr <153538780+BLACK1BAmwr@users.noreply.github.com>
2024-07-17 19:58:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Toggle menu
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+// Typing effect
+const typingSpan = document.querySelector('.typing span');
+const words = ['Frontend Developer', 'Python Developer', 'Graphic Designer', 'Anime Designer', 'Editor'];
+let wordIndex = 0;
+let letterIndex = 0;
+let currentWord = words[wordIndex];
+let isDeleting = false;
